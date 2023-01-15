508ea80fb5bab93ad5e7b1a78d88562b81c06e3a
Mai Trung Kiên <maitrungkien1910@gmail.com>
2023-01-15 19:42:43 +0700

    update

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -5,28 +5,28 @@ import NavBar from './Header/NavBar'
 import SearchBar from './Header/SearchBar'
 
 export default function Header() {
-  const location = useLocation()
-  const navigate = useNavigate()
-  const index = ['/', '/lich-thi-dau', '/ket-qua', '/du-doan'].findIndex((e) => e === location.pathname)
-  return (
-    <>
-      <div className={s.leftHeader}>
-        <SportsSoccerIcon className={s.spin} onClick={() => navigate('/')} />
-        <SearchBar />
-      </div>
-      <div className={s.middleHeader}>
-        <NavBar />
-        <span className={index >= 0 && index <= 4 ? s.underline : s.hidden} style={{ left: `${index * 25}%` }}></span>
-      </div>
-      <div className={s.rightHeader}>
-        <Link to={'./login'}>
-          <span>Đăng nhập</span>
-        </Link>
-        <span>&nbsp;hoặc&nbsp;</span>
-        <Link to={'./signup'}>
-          <span>Đăng ký</span>
-        </Link>
-      </div>
-    </>
-  )
+	const location = useLocation()
+	const navigate = useNavigate()
+	const index = ['/', '/lich-thi-dau', '/ket-qua', '/du-doan'].findIndex((e) => e === location.pathname)
+	return (
+		<>
+			<div className={s.leftHeader}>
+				<SportsSoccerIcon className={s.spin} onClick={() => navigate('/')} />
+				<SearchBar />
+			</div>
+			<div className={s.middleHeader}>
+				<NavBar />
+				<span className={index >= 0 && index <= 4 ? s.underline : s.hidden} style={{ left: `${index * 25}%` }}></span>
+			</div>
+			<div className={s.rightHeader}>
+				<Link to={'/login'}>
+					<span>Đăng nhập</span>
+				</Link>
+				<span>&nbsp;hoặc&nbsp;</span>
+				<Link to={'/signup'}>
+					<span>Đăng ký</span>
+				</Link>
+			</div>
+		</>
+	)
 }
